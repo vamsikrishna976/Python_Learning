12775577743a6432194f7bcea315a776049bedb5
vamsikrishna976 <paluruvamsikrishna16@gmail.com>
2025-05-24 17:44:15 +0530

    created list built in operatiors & list operators

---
+++ List/List_Built_in_Functions.py
@@ -0,0 +1,26 @@
+
+                                                                  #List Built-in Functions
+
+# Built-in functions that can be used with lists in Python
+# max, sum, min, list
+
+# max
+# The max() function returns the largest element in the list
+
+l1=[1,2,3,4,5]
+print(max(l1))
+
+# sum
+# The sum() function returns the sum of all elements in the list
+l1 = [1, 2, 3, 4, 5]
+print(sum(l1))
+
+# min
+# The min() function returns the smallest element in the list
+l1 = [1, 2, 3, 4, 5]
+print(min(l1))
+
+# list
+# The list() function creates a new list from an iterable (like a string or tuple)
+l2 = "Vamsi"
+print(list(l2))
---
+++ List/List_Operators.py
@@ -0,0 +1,33 @@
+
+                                                               # List operators in Python
+# List operators that can be used with lists in Python
+# Operators that can be used with lists in Python
+# +, *, in, for, len
+#concatination, repetition, membership, iteration, length
+
+#concatination
+# + operator concatenates two lists
+l1 = [1,2,3]
+l2 = [4,5,6]
+print(l1+l2)
+
+#repetition
+# * operator repeats the list a specified number of times
+l1 = [1, 2, 3]
+print(l1*5)
+
+# membership
+# in operator checks if an element is present in the list
+l1 = [1, 2, 3, 4, 5]
+print(2 in l1)
+
+# iteration
+# for operator iterates over the elements of the list
+l1 = [1, 2, 3, 4, 5]
+for i in l1 :
+    print(i)
+
+# length
+# len operator returns the number of elements in the list
+l1 = [1, 2, 3, 4, 5]
+print(len(l1))
